abe36fa21d90a52f6362bf342c05d5689080af00
Fernando Blumentals <fernando.g.blumentals@gmail.com>
2015-10-26 02:25:42 +0200

    fractal is up and running with colors

--- complex.js
+++ complex.js
@@ -10,17 +10,16 @@ complex.prototype.add = function(z) {
 
 // function for multiplying complex numbers, precisely "this" is the complex number we are multiplying another (z) to 
 complex.prototype.times = function(z) {
-    return new complex(this.real * z.real - this.real * z.imag, this.imag * z.real + this.real * z.imag);
+    return new complex(this.real * z.real - this.imag * z.imag, this.imag * z.real + this.real * z.imag);
 }
 
 complex.prototype.abs = function() {
-    return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imag, 2));
+    return Math.sqrt(this.real * this.real + this.imag * this.imag);
 }
 
 complex.prototype.toStr = function() {
-	if(this.imag < 0){
-    return this.real.toFixed(3) + " - i" + Math.abs(this.imag.toFixed(3));
-
-	}
+    if (this.imag < 0) {
+        return this.real.toFixed(3) + " - i" + Math.abs(this.imag.toFixed(3));
+    }
     return this.real.toFixed(3) + " + i" + this.imag.toFixed(3);
 }
--- julia.js
+++ julia.js
@@ -1,60 +1,60 @@
 function main() {
     var cJulia = document.getElementById('canvasJulia');
-
     cJulia.nbTerms = 150;
     cJulia.W = 4;
     cJulia.zc = new complex(0, 0); // complex numbe the center of the canvas
+    cJulia.tabColor = [];
+
+    for (var i = 0; i < cJulia.nbTerms; i++) {
+    	cJulia.tabColor.push([i*255/(cJulia.nbTerms -1),i*255/(cJulia.nbTerms -1),i*215/(cJulia.nbTerms -1) + 40, 255]);
+    };
 
     cJulia.H = cJulia.height / cJulia.width * cJulia.W;
-console.log('1');
+    console.log('1');
     cJulia.draw = function(c) {
         var context = cJulia.getContext('2d');
         var imageData = context.createImageData(cJulia.width, cJulia.height);
         var cpt = 0; // all the pixels of our canvas
         var C = 1 / 2 * (1 + Math.sqrt(1 + 4 * c.abs()));
-console.log('2');
+        console.log('2');
+        var level = 0;
 
-            for (var i = 0; i < cJulia.height; i++) {
-                for (var j = 0; j < cJulia.width; j++) {
+        for (var i = 0; i < cJulia.height; i++) {
+            for (var j = 0; j < cJulia.width; j++) {
 
 
-                    var z0 = this.canvasToComplex(i, j);
-                    var isInJuliaSet = true;
-                    //for coloring pixels
-                    for (var k = 0; k < this.nbTerms; k++) {
-                        if (z0.abs() > C) {
-                            isInJuliaSet = false;
-                            break;
-                        }
+                var z0 = this.canvasToComplex(i, j);
 
-                        z0 = z0.times(z0).add(c);
-
-                    };
-
-                    if (isInJuliaSet) {
-                        imageData.data[cpt] = 255;
-                        imageData.data[cpt + 1] = 255;
-                        imageData.data[cpt + 2] = 255;
-                        imageData.data[cpt + 3] = 255;
-
-                    } else {
-                        imageData.data[cpt] = 0;
-                        imageData.data[cpt + 1] = 0;
-                        imageData.data[cpt + 2] = 0;
-                        imageData.data[cpt + 3] = 255;
+                //for coloring pixels
+                for (var k = 0; k < this.nbTerms; k++) {
+                    level = k;
+                    if (z0.abs() > C) {
+                        isInJuliaSet = false;
+                        break;
                     }
-                    cpt = cpt + 4;
+
+                    z0 = z0.times(z0).add(c);
+
                 };
+
+                 
+                    imageData.data[cpt] = this.tabColor[level][0];
+                    imageData.data[cpt + 1] = this.tabColor[level][1];
+                    imageData.data[cpt + 2] = this.tabColor[level][2];
+                    imageData.data[cpt + 3] = this.tabColor[level][3];
+
+                cpt = cpt + 4;
             };
+        };
 
-            context.putImageData(imageData, 0, 0); // args 2 and 2 are offset
+        context.putImageData(imageData, 0, 0); // args 2 and 2 are offset
 
-        }
-
-
-        cJulia.canvasToComplex = function(li, col) { //li line of pixel, collum of pixel
-            return new complex(this.W / this.width * col + this.zc.real - this.W / 2, -this.H / this.height * li + this.zc.imag + this.H / 2);
-        }
-
-        cJulia.draw(new complex(-1.28, -1));
     }
+
+
+    cJulia.canvasToComplex = function(li, col) { //li line of pixel, collum of pixel
+        return new complex(this.W / this.width * col + this.zc.real - this.W / 2, -this.H / this.height * li + this.zc.imag + this.H / 2);
+    }
+
+    cJulia.draw(new complex(0.3, 0.5));
+}
